2d8372ab9b139c56d45b78aa322d68fe88adf731
kevin <932731452@qq.com>
2025-08-22 18:05:53 +0800

    feat：dashboard

--- app/dashboard/page.tsx
+++ app/dashboard/page.tsx
@@ -8,6 +8,11 @@ export default function Dashboard() {
   return (
     <div className="">
       <DashboardList dataList={dataList} />
+      <br></br>
+      <div className=" grid grid-cols-1 md:grid-cols-2 gap-4">
+        <div className=" bg-red-200">1</div>
+        <div className=" bg-yellow-200"> 2</div>
+      </div>
     </div>
   );
 }

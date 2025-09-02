7256bce1cabec1993daf69818d5c07a18f06589f
kevin <932731452@qq.com>
2025-09-02 11:26:31 +0800

    12

---
+++ app/test/data.ts
@@ -0,0 +1,20 @@
+
+
+export const renderData = [{
+    statue:'0', 
+    content: '123445566'
+},{
+    statue:'1', 
+    content: '34566'
+},{
+    statue:'2', 
+    content: '3455'
+}]
+
+export const statueMap = [{
+    statue: '0', value: '登记', bgColor: 'red', img: require('./img/house.png')
+}, {
+    statue: '1', value: '执行', bgColor: 'blue', img: require('./img/calendar.jpg')
+},{
+    statue: '2', value: '归档', bgColor: 'black', img: require('./img/calendar.jpg')
+},]
---
+++ app/test/page.tsx
@@ -0,0 +1,59 @@
+"use client";
+
+import React, { useRef } from "react";
+import Image from "next/image";
+import { renderData, statueMap } from "./data";
+import classNames from "classnames";
+import BgImg from "./img/house.png";
+
+const Test = () => {
+  const [show, setShow] = React.useState(false);
+  const innerRef = useRef<HTMLDivElement>(null);
+  return (
+    <div className=" flex flex-col">
+      <div
+        onClick={() => {
+          setShow(!show);
+        }}
+      >
+        增加
+      </div>
+      <div
+        style={{ height: show ? "200px" : "0px" }}
+        className=" transition-all ease-in-out duration-700 border overflow-hidden"
+      >
+        <div ref={innerRef} className=" h-[200px]">
+          123
+        </div>
+      </div>
+      <div>
+        {renderData.map((item, idx) => {
+          const data = statueMap.find((ele) => ele.statue === item.statue);
+          return (
+            <div key={idx} className=" w-[100px] h-[100px] border flex">
+              <div>
+                <div
+                  className={classNames(
+                    " bg-gradient-to-r to-[white]",
+                    `from-[${data?.bgColor}]`
+                  )}
+                >
+                  {data?.value}
+                </div>
+                <div className=" flex-1">
+                  <Image
+                    className=" w-[40px] h-[40px]"
+                    src={data?.img}
+                    alt="Landscape picture"
+                  />
+                </div>
+              </div>
+            </div>
+          );
+        })}
+      </div>
+    </div>
+  );
+};
+
+export default Test;

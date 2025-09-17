4f81f14607e2b9f05c72b54baff5a804a1a9a83d
kevin <932731452@qq.com>
2025-09-17 11:03:00 +0800

    233

--- app/select/page.tsx
+++ app/select/page.tsx
@@ -1,11 +1,12 @@
 "use client";
 
-import React, { useRef } from "react";
+import React, { useRef, useState } from "react";
 import Content, { ContentRefs } from "./components/content";
 import { data } from "./components/data";
 
 const Select = () => {
   const contentRef = useRef<ContentRefs>(null);
+  const [show, setShow] = useState(false);
   return (
     <div>
       <span
@@ -24,6 +25,28 @@ const Select = () => {
         打开
       </span>
       <Content ref={contentRef}></Content>
+      <br />
+      <div className=" w-[40px] bg-gradient-to-r from-[#000] to-[#fff] text-white">
+        div
+      </div>
+      <div
+        onClick={() => {
+          setShow(!show);
+        }}
+      >
+        点击
+      </div>
+      <div
+        style={{
+          height: show ? "100px" : "0",
+        }}
+        className=" border transform ease-in-out delay-100 duration-500 overflow-hidden"
+      >
+        <div className=" h-[100px]">
+          <div className=" bg-black opacity-50">2233</div>
+          <div className=" bg-red-500">22</div>
+        </div>
+      </div>
     </div>
   );
 };
